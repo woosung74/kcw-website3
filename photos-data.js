161fbc4c7e18c3d717eb1d88bb77d2ded2ffd5c4
woosung74 <woosung74@gmail.com>
2026-07-11 07:11:22 -0400

    Create kcw-website3

---
+++ photos-data.js
@@ -0,0 +1,70 @@
+// ============================================================
+//  웨체스터제일교회 갤러리 사진 목록
+//  사진 추가 방법:
+//    1) 사진 파일을  photos/  폴더 안에 넣는다
+//    2) 아래 목록에 한 줄 추가한다 (콤마 꼭 붙이기!)
+//
+//  cat(분류) 선택:  예배 / 친교 / 훈련 / 선교 / 청소년
+// ============================================================
+
+const PHOTOS = [
+
+  {
+    src: 'photos/gallery-outing.jpg',
+    cat: '친교',
+    caption: '교회 야외 나들이',
+    date: '2025년 가을'
+  },
+  {
+    src: 'photos/gallery-cafe.jpg',
+    cat: '청소년',
+    caption: 'Youth 카페 미니스트리',
+    date: '2025년'
+  },
+  {
+    src: 'photos/gallery-training.jpg',
+    cat: '훈련',
+    caption: '목자 훈련 학교 (Discipleship Leader Training)',
+    date: '2025년'
+  },
+  //{
+  //  src: 'photos/youth-fundraiser.jpg',
+  //  cat: '청소년',
+  //  caption: 'Youth Group Fundraiser',
+  //  date: '2025년'
+  //},
+  //{
+  //  src: 'photos/fellowship-seniors.jpg',
+  //  cat: '친교',
+  //  caption: '교회 성도님들 친교 모임',
+  //  date: '2025년'
+  //},
+
+  // ↓↓↓  새 사진은 여기 아래에 추가하세요  ↓↓↓
+  {
+    src: 'photos/youth-fundraiser.jpg',
+    cat: '청소년',
+    caption: 'Youth Group Fundraiser',
+    date: '2025년'
+  },
+  {
+    src: 'photos/fellowship-seniors.jpg',
+    cat: '친교',
+    caption: '교회 성도님들 친교 모임',
+    date: '2025년'
+  },
+  {
+    src: 'photos/youth-outing-01.jpg',
+    cat: '청소년',
+    caption: 'Youth Group Outing',
+    date: '2025년'
+  },
+
+  // {
+  //   src:     'photos/파일명.jpg',
+  //   cat:     '친교',
+  //   caption: '사진 설명',
+  //   date:    '2026년 5월'
+  // },
+
+];
